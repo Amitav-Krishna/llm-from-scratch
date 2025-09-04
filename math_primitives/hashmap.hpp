69496d896f9456ffa1bc9f843aa9893e9d7c58f8
Amitav Krishna <amtiavkrishna2011@gmail.com>
2025-09-04 19:58:05 -0400

    Implement sieve of eratosthenes in preparation for compression funct

---
+++ math_primitives/hashmap.hpp
@@ -0,0 +1,10 @@
+#ifndef HASHMAP_HPP
+#define HASHMAP_HPP
+#include "vector.hpp"
+
+// These earlier lines ensure that this file has not been included more than once in the same compilation
+
+int hash_code(vectorClass<char> chars);
+mathVector sieve_of_eratosthenes(int limit) ;
+
+#endif
